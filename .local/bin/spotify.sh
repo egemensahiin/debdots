964845ba91516212d46caffadd9a606e6703607f
egemensahiin <egemensahin95@gmail.com>
2021-01-11 17:22:42 +0300

    Added spotify widget to dwm-bar.

---
+++ .local/bin/spotify.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# # For spotify client
+# SONG_CODE=$(xdotool search --class Spotify | tail -n 1)
+# SONG_NAME=$(xdotool getwindowname ${SONG_CODE})
+# if [[ $SONG_NAME != "" ]]
+# then
+#   echo " ${SONG_NAME} | "
+# fi
+
+# For spotify-tui
+SONG=$(spotify-tui playback | awk -F " " '{print $NF}')
+STATUS=$(echo $SONG | awk '{print $1}')
+# if [[ $SONG == "" ]]
+# then
+#   echo ""
+#   exit 0
+# else
+case "$STATUS" in
+  契) echo " $(echo $SONG | awk -F "契" '{print $NF}') |"; exit 0 ;;
+  *) echo ""; exit 0 ;;
+esac
+# fi
