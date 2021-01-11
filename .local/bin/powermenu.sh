fa373cb98209b25d5821f1ef734ac4af9e81ce32
egemensahiin <egemensahin95@gmail.com>
2021-01-11 17:20:58 +0300

    Added suspention to power menu.

--- .local/bin/powermenu.sh
+++ .local/bin/powermenu.sh
@@ -2,10 +2,11 @@
 declare -A CMDS
 CMDS=(
   [" shutdown"]="systemctl poweroff"
+  ["鈴 suspend"]="systemctl suspend"
   [" reboot"]="systemctl reboot"
   [" exit session"]="pkill dwm"
   [" lock screen"]="/home/egemen/.local/bin/locker.sh"
-  [" quit menu"]="pkill rofi"
+#  [" quit menu"]="pkill rofi"
 )
 
 gen_list() {
@@ -16,7 +17,7 @@ gen_list() {
 }
 
 main() {
-  power_menu=$( (gen_list) | rofi -dmenu -matching fuzzy -lines 5 -no-custom location 0 -p "power menu" )
+  power_menu=$( (gen_list) | rofi -dmenu -font "Inconsolata Nerd Font 13" -matching fuzzy -lines 5 -no-custom location 0 -p "power menu" )
   if [[ -n "${power_menu}" ]]
   then
     eval "${CMDS[$power_menu]}"
